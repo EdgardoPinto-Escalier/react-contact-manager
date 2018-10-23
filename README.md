e2201707fbc7878dbac8a712a395f4a2b8aa1472
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-10-23 23:28:44 +0200

    Readme file updated

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # REACT CONTACT MANAGER
-Simple contact manager web application made with React
+
+Simple contact manager web application made with React. This is a static React application that gets fake data from https://jsonplaceholder.typicode.com using axios for the http requests. React Contact Manager is a CRUD application and the following React concepts has been used to create this simple application:
+
+- Components, JSX and Props
+- State and Context API
+- Working with forms
+- Form validations
+- React Router
+- Lifecycle and HTTP requests
+- GET, POST, DELETE and PUT requests
+- Using Async/Await
+
+## DEMO
+
+[REACT CONTACT MANAGER](https://edgardopinto-escalier.github.io/react-contact-manager/#/)
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,7 +7,23 @@ export default () => {
         <i className="fas fa-info-circle" /> ABOUT{" "}
         <span className="text-primary">REACT CONTACT MANAGER</span>
       </h1>
-      <p className="lead">Simple React contact manager application</p>
+      <p className="lead">
+        Simple contact manager web application made with React. This is a static
+        React application that gets fake data from
+        https://jsonplaceholder.typicode.com using axios for the http requests.
+        React Contact Manager is a CRUD application and the following React
+        concepts has been used to create this simple application:
+      </p>
+      <ul>
+        <li>Components, JSX and Props</li>
+        <li>State and Context API</li>
+        <li>Working with forms</li>
+        <li>Form validations</li>
+        <li>React Router</li>
+        <li>Lifecycle and HTTP requests</li>
+        <li>GET, POST, DELETE and PUT requests</li>
+        <li>Using Async/Await</li>
+      </ul>
       <p className="text-secondary">Version 1.0.0</p>
     </div>
   );
