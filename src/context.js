fc7e0cc164b917c629cbe5d48e43cc490b31a707
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-10-23 18:34:44 +0200

    Performed refactoring on the http requests

--- src/context.js
+++ src/context.js
@@ -1,56 +1,44 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import axios from "axios";
 
 const Context = React.createContext();
 
 const reducer = (state, action) => {
-  switch(action.type) {
-    case 'DELETE_CONTACT':
+  switch (action.type) {
+    case "DELETE_CONTACT":
       return {
         ...state,
-        contacts: state.contacts.filter(contact => contact.id !== action.payload)
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
       };
-      case 'ADD_CONTACT':
+    case "ADD_CONTACT":
       return {
         ...state,
         contacts: [action.payload, ...state.contacts]
       };
-    default: 
+    default:
       return state;
   }
 };
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'Peter Thomas',
-        email: 'petert@mail.com',
-        phone: '523-3332-5236'
-      },
-      {
-        id: 2,
-        name: 'Laura Williams',
-        email: 'laura@gmail.com',
-        phone: '111-222-3333'
-      },
-      {
-        id: 3,
-        name: 'Sean Peters',
-        email: 'sean1@yahoo.com',
-        phone: '222-333-4444'
-      }
-    ],
+    contacts: [],
     dispatch: action => this.setState(state => reducer(state, action))
   };
+
+  async componentDidMount() {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+    this.setState({ contacts: res.data });
+  }
 
   render() {
     return (
       <Context.Provider value={this.state}>
         {this.props.children}
       </Context.Provider>
-    )
+    );
   }
 }
 
