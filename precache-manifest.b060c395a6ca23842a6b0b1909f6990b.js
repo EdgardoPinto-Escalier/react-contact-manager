1277ad57e123ea929a1d04aeb457e0502c753d66
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-10-23 22:57:59 +0200

    Updates

---
+++ precache-manifest.b060c395a6ca23842a6b0b1909f6990b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4edbdc0eee6dc393dfd6",
+    "url": "/static/js/main.4edbdc0e.chunk.js"
+  },
+  {
+    "revision": "bb6765bb5f5251c47b02",
+    "url": "/static/js/1.bb6765bb.chunk.js"
+  },
+  {
+    "revision": "4edbdc0eee6dc393dfd6",
+    "url": "/static/css/main.395b9295.chunk.css"
+  },
+  {
+    "revision": "052d475711ae8e0b94c287f6c3786efb",
+    "url": "/index.html"
+  }
+];
