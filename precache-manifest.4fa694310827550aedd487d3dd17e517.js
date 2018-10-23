5f605530e121673e4cc92d266cd02e0fabf58fde
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-10-23 23:05:38 +0200

    Updates

---
+++ precache-manifest.4fa694310827550aedd487d3dd17e517.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac3c48b9cdbff86df9e4",
+    "url": "/react-contact-manager/static/js/runtime~main.ac3c48b9.js"
+  },
+  {
+    "revision": "1887479c1abd6c9e1c19",
+    "url": "/react-contact-manager/static/js/main.1887479c.chunk.js"
+  },
+  {
+    "revision": "bb6765bb5f5251c47b02",
+    "url": "/react-contact-manager/static/js/1.bb6765bb.chunk.js"
+  },
+  {
+    "revision": "1887479c1abd6c9e1c19",
+    "url": "/react-contact-manager/static/css/main.395b9295.chunk.css"
+  },
+  {
+    "revision": "77d45ac31c2da55867eae9d09206ca6e",
+    "url": "/react-contact-manager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b060c395a6ca23842a6b0b1909f6990b.js"
+  "/react-contact-manager/precache-manifest.4fa694310827550aedd487d3dd17e517.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-contact-manager/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
